c2c5f8778522c4d27ec6f6844e47250b78187c1e
Pranjali Joshi <pranjali06kjoshi@gmail.com>
2021-09-30 09:45:46 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+import ScanScreen from './screens/ScanScreen';
+
+
+export default class App extends React.Component {
+  render(){
+    return (
+      
+        <ScanScreen />
+      
+    );
+  }
+}
+
